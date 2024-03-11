1cfcc06b65b5a44d3dd127679e130f60fd87b51e
mindsgn <seni.tembe@protonmail.com>
2024-03-11 09:53:54 +0200

    update

--- mixtapmonkey.py
+++ mixtapmonkey.py
@@ -31,48 +31,55 @@ def get_page(page_number):
 
 
 def go_to_artist_page(artist_page):
-    response = requests.get("https://mixtapemonkey.com"+artist_page)
-    content = BeautifulSoup(response.content, 'html.parser')
-    container = content.select(".u-full-width")
-    mixtapes = container[0].select("a")
-    for mixtape in mixtapes:
-        go_to_mixtape_page(mixtape.attrs["href"])
+    try:
+        response = requests.get("https://mixtapemonkey.com"+artist_page)
+        content = BeautifulSoup(response.content, 'html.parser')
+        container = content.select(".u-full-width")
+        mixtapes = container[0].select("a")
+        for mixtape in mixtapes:
+            go_to_mixtape_page(mixtape.attrs["href"])
+    except Exception as e:
+        pass
+    
 
 
 def go_to_mixtape_page(mixtape_page):
-    response = requests.get("https://mixtapemonkey.com"+mixtape_page)
-    content = BeautifulSoup(response.content, 'html.parser')
-    mixtape_image = content.select_one(".mixtape-thumb")
-    download = content.select_one(".download")
-    album_link = download.attrs["href"]
-    image = mixtape_image.select_one("img")
-    cover_art = "https://mixtapemonkey.com"+image.attrs["src"]
+    try:
+        response = requests.get("https://mixtapemonkey.com"+mixtape_page)
+        content = BeautifulSoup(response.content, 'html.parser')
+        mixtape_image = content.select_one(".mixtape-thumb")
+        download = content.select_one(".download")
+        album_link = download.attrs["href"]
+        image = mixtape_image.select_one("img")
+        cover_art = "https://mixtapemonkey.com"+image.attrs["src"]
 
-    album_title = content.select_one("h1").text
-    artist_container = content.select_one("h2")
-    artist_link = artist_container.select_one("a")
-    artist = artist_link.text
+        album_title = content.select_one("h1").text
+        artist_container = content.select_one("h2")
+        artist_link = artist_container.select_one("a")
+        artist = artist_link.text
 
-    save_album(artist, album_title, cover_art, album_link)
-    media_player = content.select_one(".mediatec-cleanaudioplayer")
-    tracks = media_player.select("li")
+        save_album(artist, album_title, cover_art, album_link)
+        media_player = content.select_one(".mediatec-cleanaudioplayer")
+        tracks = media_player.select("li")
 
-    for track in tracks:
-        title = track.attrs["data-title"]
-        track_link = "https://mixtapemonkey.com"+track.attrs["data-url"]
+        for track in tracks:
+            title = track.attrs["data-title"]
+            track_link = "https://mixtapemonkey.com"+track.attrs["data-url"]
 
-        match = re.match(r'^(\d+)\s*-\s*(.*)$', title)
-        track_number = int(match.group(1)) if match else None
-        title = match.group(2).strip() if match else None
+            match = re.match(r'^(\d+)\s*-\s*(.*)$', title)
+            track_number = int(match.group(1)) if match else None
+            title = match.group(2).strip() if match else None
 
-        save_track(
-            artist,
-            title,
-            track_number,
-            track_link,
-            album_title,
-            cover_art
-        )
+            save_track(
+                artist,
+                title,
+                track_number,
+                track_link,
+                album_title,
+                cover_art
+            )
+    except Exception as e:
+        pass
 
 
 def save_album(artist, album_title, cover_art, album_link):
